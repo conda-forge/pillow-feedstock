958d8e00c0e4a1af501ca96cb646602767e219af
H. Vetinari <h.vetinari@gmx.com>
2022-01-05 17:50:17 +1100

    add other libs that pillow offers functionality for

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,9 +3,10 @@
 export JPEG_ROOT=$PREFIX
 export JPEG2K_ROOT=$PREFIX
 export ZLIB_ROOT=$PREFIX
-# export IMAGEQUANT_ROOT=None
+export IMAGEQUANT_ROOT=$PREFIX
 export TIFF_ROOT=$PREFIX
 export FREETYPE_ROOT=$PREFIX
+export FRIBIDI_ROOT=$PREFIX
 export LCMS_ROOT=$PREFIX
 
 $PYTHON -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
