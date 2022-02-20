6030930de66ad2f2548003e8860f0d8907674fcc
H. Vetinari <h.vetinari@gmx.com>
2022-02-20 12:15:02 +1100

    disable fribidi and libimagequant pending GPL discussion

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,10 +3,11 @@
 export JPEG_ROOT=$PREFIX
 export JPEG2K_ROOT=$PREFIX
 export ZLIB_ROOT=$PREFIX
-export IMAGEQUANT_ROOT=$PREFIX
+# currently disabled, see meta.yaml
+# export IMAGEQUANT_ROOT=$PREFIX
 export TIFF_ROOT=$PREFIX
 export FREETYPE_ROOT=$PREFIX
-export FRIBIDI_ROOT=$PREFIX
+# export FRIBIDI_ROOT=$PREFIX
 export LCMS_ROOT=$PREFIX
 export WEBP_ROOT=$PREFIX
 export XCB_ROOT=$PREFIX
